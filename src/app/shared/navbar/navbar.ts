bdfe6b73910e864388e6989827c704a1a36df476
Doug-26 <cjdelacruz267@gmail.com>
2025-07-06 23:31:05 +0800

    Add navbar component with routing and cart item count

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CartService } from '../../services/cart';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
+
+  private cartService = inject(CartService);
+  totalItems = this.cartService.totalItems;
 
 }
